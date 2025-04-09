f84c802729753ac6d3af9caa6eaefccb49b068d9
ranjith <ranjithvcc@gmail.com>
2025-04-09 23:33:38 +0000

    solution

---
+++ src/components/TextEditor/styledComponent.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const BoldBtn = styled.button`
+color: ${props => (props.active === true ? '#faff00' : '#f1f5f9')};
+background-color: transparent;
+outline: none;
+cursor: pointer;
+border: none;
+`
+
+export const ItalicBtn = styled(BoldBtn)`
+color: ${props => (props.active === true ? '#faff00' : '#f1f5f9')};
+`
+
+export const UnderlineBtn = styled(BoldBtn)`
+color: ${props => (props.active === true ? '#faff00' : '#f1f5f9')};
+`
+
+export const TextArea = styled.textArea`
+font-weight: ${props => (props.activeBold ? 'bold' : 'normal')};
+font-style: ${props => (props.activeItalic ? 'itaalic' : 'normal')}
+text-decoration: ${props =>
+  props.activeUnderline === true ? 'underline' : 'normal'};`
